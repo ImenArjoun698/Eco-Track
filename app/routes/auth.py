27d57c14d8f557115b7707d546e6b0dc3419261b
ImenArjoun698 <imenarjoun40@gmail.com>
2026-02-14 13:26:35 +0100

    Update auth.py

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -4,11 +4,12 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return render_template("login.html")
+    return render_template('login.html')
 
 @auth.route('/register')
 def register():
-    return render_template("register.html")
+    return render_template('register.html')
+
 
 
 
